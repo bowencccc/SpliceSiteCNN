0c33fc2c2542cc74ba03b5bc5776f059dad14684
bowencccc <43017920+bowencccc@users.noreply.github.com>
2025-06-16 13:15:34 -0400

    Update evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -4,7 +4,7 @@ import numpy as np
 import re
 import time
 import matplotlib.pyplot as plt
-
+import os
 
 def parse_gtf(gtf_file):
     gtf_records = []
@@ -42,6 +42,16 @@ def parse_markov_score(markov_score_file):
                 data.append((transcript_id, float(score5)))
     return pd.DataFrame(data, columns=['Transcripts', 'Markov_Score'])
 
+def extract_reference_mrna_count(stats_file_path):
+    with open(stats_file_path, "r") as f:
+        for line in f:
+            match = re.search(r"Reference mRNAs\s*:\s*(\d+)", line)
+            if match:
+                count = int(match.group(1))
+                print(f"✅ Reference mRNAs: {count}")
+                return count
+    print("❌ Reference mRNAs not found.")
+    return None
 
 
 def main():
@@ -53,6 +63,8 @@ def main():
                         help="Path to markov score file")
     parser.add_argument("--annotated_gtf", type=str, required=True,
                         help="Path to annotated gtf file")
+    parser.add_argument("--gffcompare_stats", type=str, required=True,
+                        help="Path to gffcompare stats file")
     parser.add_argument("--output_file", type=str, required=True,
                         help="Output file name (species-specific)")
 
@@ -64,8 +76,17 @@ def main():
         tsv_dfs.append(df)
     markov_score = args.markov_score
     annotated_gtf = args.annotated_gtf
+    gffcompare_stats = args.gffcompare_stats
     output_file = args.output_file
+    print(f"Input files: {args.score_tsv_path}, {markov_score}, {annotated_gtf}, {gffcompare_stats}")
 
+ 
+    
+    basename = os.path.basename(annotated_gtf)
+    species = basename.split('_')[0]
+    print(f"Species: {species}")
+
+    reference_mrna_count = extract_reference_mrna_count(gffcompare_stats)
     
     gtf_df = parse_gtf(annotated_gtf)
 
@@ -84,19 +105,21 @@ def main():
         class_code_cnn = (merged_cnn_df['class_code'] == '=')
         merged_cnn_df['label'] = class_code_cnn.astype(int)
         thresholds_cnn = sorted(merged_cnn_df['Junction_Score'].unique())
-        thresholds_cnn = [x for x in thresholds_cnn if x < -5]
+        
         precision_list_cnn = []
         recall_list_cnn = []
         y_true_cnn = merged_cnn_df['label'].values
         y_scores_cnn = merged_cnn_df['Junction_Score'].values
 
+        thr_pct   = np.nanpercentile(y_scores_cnn, 20)
+        thresholds_cnn = [x for x in thresholds_cnn if x < thr_pct]
         for threshold in thresholds_cnn:
             y_pred_cnn = (y_scores_cnn >= threshold).astype(int)
             TP = np.sum((y_true_cnn == 1) & (y_pred_cnn == 1))
             FP = np.sum((y_true_cnn == 0) & (y_pred_cnn == 1))
             FN = np.sum((y_true_cnn == 1) & (y_pred_cnn == 0))
             precision = TP / (TP + FP)
-            recall = TP / (35704 - 3416)
+            recall = TP / reference_mrna_count
             precision_list_cnn.append(precision)
             recall_list_cnn.append(recall)
         
@@ -107,18 +130,21 @@ def main():
     class_code_markov = (merged_markov_df['class_code'] == '=')
     merged_markov_df['label'] = class_code_markov.astype(int)
     thresholds_markov = sorted(merged_markov_df['Markov_Score'].unique())
-    thresholds_markov = [x for x in thresholds_markov if x < 7]
+    
     precision_list_markov = []
     recall_list_markov = []
     y_true_markov = merged_markov_df['label'].values
     y_scores_markov = merged_markov_df['Markov_Score'].values
+    thr_pct = np.nanpercentile(y_scores_markov, 20)
+    thresholds_markov = [x for x in thresholds_markov if x < thr_pct]
+
     for threshold in thresholds_markov:
         y_pred_markov = (y_scores_markov >= threshold).astype(int)
         TP = np.sum((y_true_markov == 1) & (y_pred_markov == 1))
         FP = np.sum((y_true_markov == 0) & (y_pred_markov == 1))
         FN = np.sum((y_true_markov == 1) & (y_pred_markov == 0))
         precision = TP / (TP + FP)
-        recall = TP / (35704 - 3416)
+        recall = TP / reference_mrna_count
         precision_list_markov.append(precision)
         recall_list_markov.append(recall)
 
@@ -129,7 +155,7 @@ def main():
         plt.plot(recall_lists_cnn[i], precision_lists_cnn[i], label=f'CNN Model {i+1}')
     plt.xlabel('Recall')
     plt.ylabel('Precision')
-    plt.title('Precision-Recall Curve for filtering transcripts')
+    plt.title(f'{species} Precision-Recall Curve for filtering transcripts')
     plt.grid(True)
     plt.legend()
     plt.tight_layout()
